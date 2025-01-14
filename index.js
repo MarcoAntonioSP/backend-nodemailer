bd25bd0cb5bde5e3b1af3f594370a0183d63d259
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-14 02:10:45 -0300

    ajustecors5656565

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://www.lccopper.com",
   "https://template-nextjs-flowbite-tailwind.vercel.app",
-  "https://template-nextjs-flo-git-f7b41a-marco-antonios-projects-796d869d.vercel.app", // Adicione este domínio
+  "https://template-nextjs-flo-git-f7b41a-marco-antonios-projects-796d869d.vercel.app" // Adicione este domínio
 ];
 
 const corsOptions = {
@@ -27,18 +27,9 @@ const corsOptions = {
   credentials: true, // Permite cookies e cabeçalhos de autenticação
   optionsSuccessStatus: 200,
 };
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
-app.options("*", cors(corsOptions));
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Responder a solicitações preflight
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -53,13 +44,11 @@ const captchaLimiter = rateLimit({
 const emailLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hora
   max: 2, // Limita a 2 envios de e-mails por IP por hora
-  message:
-    "Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.",
+  message: "Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.",
   statusCode: 429, // Status HTTP para limite atingido
   handler: (req, res /*next*/) => {
     res.status(429).send({
-      error:
-        "Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.",
+      error: "Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.",
     });
   },
 });
@@ -86,8 +75,7 @@ app.get("/captcha", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { name, company, email, phone, message, captchaId, captchaAnswer } =
-    req.body;
+  const { name, company, email, phone, message, captchaId, captchaAnswer } = req.body;
 
   // Validação de campos obrigatórios
   if (!name || !email || !message || !captchaId || !captchaAnswer) {
@@ -111,6 +99,10 @@ app.post("/send", (req, res) => {
     smtpUser = process.env.LCCOPPER_USER_EMAIL;
     smtpPass = process.env.LCCOPPER_USER_PASSWORD;
     toEmail = process.env.LCCOPPER_TO_EMAIL;
+  } else if (origin === "https://template-nextjs-flo-git-f7b41a-marco-antonios-projects-796d869d.vercel.app") {
+    smtpUser = process.env.TEMPLATE_USER_EMAIL;
+    smtpPass = process.env.TEMPLATE_USER_PASSWORD;
+    toEmail = process.env.TEMPLATE_TO_EMAIL;
   } else {
     return res.status(400).send({ error: "Origem inválida" });
   }
@@ -121,30 +113,24 @@ app.post("/send", (req, res) => {
     auth: { user: smtpUser, pass: smtpPass },
   });
 
-  transporter
-    .sendMail({
-      from: smtpUser,
-      to: toEmail,
-      replyTo: email,
-      subject: `Contato de ${name} - ${company}`,
-      text: `Nome: ${name}\nEmpresa: ${company}\nE-mail: ${email}\nTelefone: ${phone}\n\nMensagem:\n${message}`,
-    })
-    .then((info) => {
-      console.log("E-mail enviado com sucesso:", info);
-      res.send(info);
-    })
-    .catch((error) => {
-      console.error("Erro ao enviar e-mail:", error);
-      res
-        .status(500)
-        .send({
-          error: "Erro ao enviar o e-mail. Tente novamente mais tarde.",
-        });
-    });
+  transporter.sendMail({
+    from: smtpUser,
+    to: toEmail,
+    replyTo: email,
+    subject: `Contato de ${name} - ${company}`,
+    text: `Nome: ${name}\nEmpresa: ${company}\nE-mail: ${email}\nTelefone: ${phone}\n\nMensagem:\n${message}`,
+  }).then((info) => {
+    console.log("E-mail enviado com sucesso:", info);
+    res.send(info);
+  }).catch((error) => {
+    console.error("Erro ao enviar e-mail:", error);
+    res.status(500).send({ error: "Erro ao enviar o e-mail. Tente novamente mais tarde." });
+  });
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
 // Exporte a função para o Vercel
 module.exports = app;
