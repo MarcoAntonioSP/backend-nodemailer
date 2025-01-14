1e7ced5314682b8fabad5957c08350db1bc61eff
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-14 05:41:26 -0300

    wqeiuqwioequweiouwe

--- index.js
+++ index.js
@@ -1,95 +1,105 @@
-const rateLimit = require('express-rate-limit');
-const express = require('express');
-const app = express();
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+// const rateLimit = require('express-rate-limit');
+// const express = require('express');
+// const app = express();
+// const nodemailer = require('nodemailer');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// require('dotenv').config();
 
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:3000/',
-  'https://www.lccopper.com',
-  'https://template-nextjs-flowbite-tailwind.vercel.app',
-];
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'http://localhost:3000/',
+//   'https://www.lccopper.com',
+//   'https://template-nextjs-flowbite-tailwind.vercel.app',
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: 'GET,POST,PUT,DELETE',
+//   allowedHeaders: 'Content-Type,Authorization',
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors(corsOptions));
 
-const emailLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 2,
-  message: 'Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.',
-  statusCode: 429,
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/send', emailLimiter);
-app.set('trust proxy', 1);
+// const emailLimiter = rateLimit({
+//   windowMs: 60 * 60 * 1000,
+//   max: 2,
+//   message: 'Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.',
+//   statusCode: 429,
+// });
 
-app.get('/', (req, res) => {
-  res.send('API para gestão de formulários de e-mail.');
-});
+// app.use('/send', emailLimiter);
+// app.set('trust proxy', 1);
 
-app.post('/send', (req, res) => {
-  const { name, company, email, phone, message } = req.body;
+// // Lidar com a requisição OPTIONS (preflight request)
+// app.options('/send', cors(corsOptions));
 
-  const origin = req.get('origin');
-  console.log('Origem recebida:', origin);
+// app.get('/', (req, res) => {
+//   res.send('API para gestão de formulários de e-mail.');
+// });
 
-  let smtpUser, smtpPass, toEmail;
+// // Endpoint de envio de e-mail
+// app.post('/send', (req, res) => {
+//   const { name, company, email, phone, message } = req.body;
 
-  if (origin === 'http://localhost:3000') {
-    smtpUser = process.env.LOCALHOST_USER_EMAIL;
-    smtpPass = process.env.LOCALHOST_USER_PASSWORD;
-    toEmail = process.env.LOCALHOST_TO_EMAIL;
-  } else if (origin === 'https://www.lccopper.com') {
-    smtpUser = process.env.LCCOPPER_USER_EMAIL;
-    smtpPass = process.env.LCCOPPER_USER_PASSWORD;
-    toEmail = process.env.LCCOPPER_TO_EMAIL;
-  } else if (origin === 'https://template-nextjs-flowbite-tailwind.vercel.app') {
-    smtpUser = process.env.TEMPLATE_USER_EMAIL;
-    smtpPass = process.env.TEMPLATE_USER_PASSWORD;
-    toEmail = process.env.TEMPLATE_TO_EMAIL;
-  } else {
-    return res.status(400).json({ error: 'Origem inválida' });
-  }
+//   const origin = req.get('origin');
+//   console.log('Origem recebida:', origin);
 
-  console.log('Credenciais SMTP:', { smtpUser, smtpPass, toEmail });
+//   let smtpUser, smtpPass, toEmail;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.umbler.com",
-    port: 587,
-    auth: { user: smtpUser, pass: smtpPass },
-  });
+//   if (origin === 'http://localhost:3000') {
+//     smtpUser = process.env.LOCALHOST_USER_EMAIL;
+//     smtpPass = process.env.LOCALHOST_USER_PASSWORD;
+//     toEmail = process.env.LOCALHOST_TO_EMAIL;
+//   } else if (origin === 'https://www.lccopper.com') {
+//     smtpUser = process.env.LCCOPPER_USER_EMAIL;
+//     smtpPass = process.env.LCCOPPER_USER_PASSWORD;
+//     toEmail = process.env.LCCOPPER_TO_EMAIL;
+//   } else if (origin === 'https://template-nextjs-flowbite-tailwind.vercel.app') {
+//     smtpUser = process.env.TEMPLATE_USER_EMAIL;
+//     smtpPass = process.env.TEMPLATE_USER_PASSWORD;
+//     toEmail = process.env.TEMPLATE_TO_EMAIL;
+//   } else {
+//     return res.status(400).json({ error: 'Origem inválida' });
+//   }
 
-  transporter.sendMail({
-    from: smtpUser,
-    to: toEmail,
-    replyTo: email,
-    subject: `Contato de ${name} - ${company}`,
-    text: `Nome: ${name}\nEmpresa: ${company}\nE-mail: ${email}\nTelefone: ${phone}\n\nMensagem:\n${message}`,
-  }).then(info => {
-    res.json({ success: true, info });
-  }).catch(error => {
-    console.error('Erro ao enviar o e-mail:', error);
-    res.status(500).json({ error: 'Erro ao enviar o e-mail.' });
-  });
-});
+//   console.log('Credenciais SMTP:', { smtpUser, smtpPass, toEmail });
 
-module.exports = app;
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.umbler.com",
+//     port: 587,
+//     auth: { user: smtpUser, pass: smtpPass },
+//   });
+
+//   transporter.sendMail({
+//     from: smtpUser,
+//     to: toEmail,
+//     replyTo: email,
+//     subject: `Contato de ${name} - ${company}`,
+//     text: `Nome: ${name}\nEmpresa: ${company}\nE-mail: ${email}\nTelefone: ${phone}\n\nMensagem:\n${message}`,
+//   }).then(info => {
+//     res.json({ success: true, info });
+//   }).catch(error => {
+//     console.error('Erro ao enviar o e-mail:', error);
+//     res.status(500).json({ error: 'Erro ao enviar o e-mail.' });
+//   });
+// });
+
+// // Iniciar o servidor
+// app.listen(port, () => {
+//   console.log(`Servidor rodando na porta ${port}`);
+// });
+
+// module.exports = app;
