81360f39a26ce5a0d5fb16a61ba89ed07fca009f
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-14 02:40:31 -0300

    askjdasklqwuioeuiqw

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://www.lccopper.com',
   'https://template-nextjs-flowbite-tailwind.vercel.app',
-  'https://template-nextjs-flo-git-f7b41a-marco-antonios-projects-796d869d.vercel.app', // Adicione este domínio
+  'https://template-nextjs-flo-git-f7b41a-marco-antonios-projects-796d869d.vercel.app',
 ];
-
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -25,20 +24,13 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, // Permite cookies e cabeçalhos de autenticação
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Limitação de tentativas de Captcha por IP
-const captchaLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 5, // Limita a 5 tentativas de obter uma pergunta de captcha por IP
-  message: 'Muitas requisições para o Captcha, tente novamente mais tarde.',
-});
 
 // Limitação de envio de e-mails por IP
 const emailLimiter = rateLimit({
@@ -53,39 +45,19 @@ const emailLimiter = rateLimit({
   },
 });
 
-// Aplicar as limitações nos respectivos endpoints
-app.use('/captcha', captchaLimiter); // Limitar tentativas de Captcha
-app.use('/send', emailLimiter); // Limitar envios de e-mails
-
-// Perguntas e respostas armazenadas como variáveis de ambiente
-const questions = process.env.CAPTCHA_QUESTIONS.split('|').map(q => {
-  const [question, answer] = q.split(';');
-  return { question, answer };
-});
+// Aplicar a limitação no endpoint de envio de e-mails
+app.use('/send', emailLimiter);
 
 app.get('/', (req, res) => {
   res.send('Api para gestão de formularios de email');
 });
 
-// Endpoint para obter uma pergunta aleatória do captcha
-app.get('/captcha', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  const question = questions[randomIndex];
-  res.send({ question: question.question, id: randomIndex });
-});
-
 app.post('/send', (req, res) => {
-  const { name, company, email, phone, message, captchaId, captchaAnswer } = req.body;
+  const { name, company, email, phone, message } = req.body;
 
   // Validação de campos obrigatórios
-  if (!name || !email || !message || !captchaId || !captchaAnswer) {
+  if (!name || !email || !message) {
     return res.status(400).send({ error: 'Todos os campos são obrigatórios.' });
-  }
-
-  // Validar resposta do captcha
-  const correctAnswer = questions[captchaId]?.answer;
-  if (!correctAnswer || captchaAnswer !== correctAnswer) {
-    return res.status(400).send({ error: 'Captcha inválido' });
   }
 
   // Configurar credenciais de envio com base na origem
@@ -125,7 +97,6 @@ app.post('/send', (req, res) => {
       res.status(500).send({ error: 'Erro ao enviar o e-mail. Tente novamente mais tarde.' });
     });
 });
-
 
 // Exporte a função para o Vercel
 module.exports = app;
