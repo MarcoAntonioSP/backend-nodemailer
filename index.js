22aae2bae86bcb49c3640f77df5f6e0039c79ff5
MarcoAgostinoMelo <marco.agostinoo@hotmail.com>
2025-01-15 01:33:53 -0300

    daskjldsalkdqwieouq

--- index.js
+++ index.js
@@ -1,67 +1,89 @@
-const rateLimit = require('express-rate-limit');
-const express = require('express');
+const rateLimit = require("express-rate-limit");
+const express = require("express");
 const app = express();
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
-
-// Configuração CORS consolidada
-const allowedOrigins = [
-  'http://localhost:3000/',
-  'https://www.lccopper.com',
-  'https://template-nextjs-flowbite-tailwind.vercel.app/',
-  'https://template-nextjs-flowbite-tailwind.vercel.app',
-  'https://template-nextjs-flowbite-tailwind.vercel.app/pages/contato';
-];
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 const corsOptions = {
-  origin: allowedOrigins,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Adicione OPTIONS
-  allowedHeaders: 'Content-Type,Authorization',
-  preflightContinue: true, // Permita redirecionamentos na pré-requisição
-  optionsSuccessStatus: 200, // Altere para 200 para indicar sucesso
+  origin: (origin, callback) => {
+    const allowedOriginsPatterns = [
+      /^http:\/\/localhost:\d+$/,
+      /^https:\/\/template-nextjs-flowbite-tailwind\.vercel\.app$/,
+      /^https:\/\/www\.lccopper\.com$/,
+    ];
+
+    const isAllowed = allowedOriginsPatterns.some((pattern) =>
+      pattern.test(origin)
+    );
+    callback(null, isAllowed);
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+  preflightContinue: true,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-
-// Middleware para parsing do corpo da requisição
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Limitação de envio de e-mails por IP
 const emailLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hora
-  max: 2, // Limite: 2 envios de e-mails por hora
-  message: 'Você atingiu o limite de envio de e-mails (2) por hora. Tente novamente em uma hora.',
+  windowMs: 60 * 60 * 1000,
+  max: 2,
+  message:
+    "Limite de envio de e-mails atingido! Por favor, aguarde 1 hora antes de tentar novamente.",
   statusCode: 429,
 });
 
-// Aplicar limitações nos endpoints
-app.use('/send', emailLimiter);
-
-app.get('/', (req, res) => {
-  res.send('API para gestão de formulários de e-mail.');
+app.get("/", (req, res) => {
+  res.send("API para gestão de formulários de e-mail.");
 });
 
-// Endpoint para envio de e-mails (sem CAPTCHA)
-app.post('/send', (req, res) => {
+app.post("/send", emailLimiter, (req, res) => {
   const { name, company, email, phone, message } = req.body;
 
-  // Configuração SMTP com base na origem
-  const origin = req.get('origin');
+  if (
+    !name ||
+    !email ||
+    !message ||
+    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) ||
+    !/^\(?([0-9]{2})\)?[-. ]?([0-9]{4,5})[-. ]?([0-9]{4})$/.test(phone)
+  ) {
+    return res.status(400).json({
+      error:
+        "Dados inválidos. Por favor, verifique o nome, e-mail, telefone e mensagem.",
+    });
+  }
+
+  const origin = req.get("origin");
   let smtpUser, smtpPass, toEmail;
 
-  if (origin === 'https://template-nextjs-flowbite-tailwind.vercel.app/') {
+  if (origin === "https://template-nextjs-flowbite-tailwind.vercel.app") {
     smtpUser = process.env.LOCALHOST_USER_EMAIL;
     smtpPass = process.env.LOCALHOST_USER_PASSWORD;
     toEmail = process.env.LOCALHOST_TO_EMAIL;
-  } else if (origin === 'http://localhost:3000') {
+  } else if (origin === "http://localhost:3000") {
     smtpUser = process.env.LCCOPPER_USER_EMAIL;
     smtpPass = process.env.LCCOPPER_USER_PASSWORD;
     toEmail = process.env.LCCOPPER_TO_EMAIL;
   } else {
-    return res.status(400).json({ error: 'Origem inválida' });
+    return res.status(400).json({ error: "Origem inválida" });
+  }
+
+  if (
+    !smtpUser ||
+    !smtpPass ||
+    !toEmail ||
+    typeof smtpUser !== "string" ||
+    typeof smtpPass !== "string" ||
+    typeof toEmail !== "string"
+  ) {
+    console.error("Credenciais SMTP inválidas para a origem:", origin);
+    return res.status(500).json({
+      error: "Erro interno. Por favor, tente novamente mais tarde.",
+    });
   }
 
   const transporter = nodemailer.createTransport({
@@ -70,18 +92,31 @@ app.post('/send', (req, res) => {
     auth: { user: smtpUser, pass: smtpPass },
   });
 
-  transporter.sendMail({
-    from: smtpUser,
-    to: toEmail,
-    replyTo: email,
-    subject: `Contato de ${name} - ${company}`,
-    text: `Nome: ${name}\nEmpresa: ${company}\nE-mail: ${email}\nTelefone: ${phone}\n\nMensagem:\n${message}`,
-  }).then(info => {
-    res.json({ success: true, info });
-  }).catch(error => {
-    console.error('Erro ao enviar o e-mail:', error);
-    res.status(500).json({ error: 'Erro ao enviar o e-mail.' });
-  });
+  transporter
+    .sendMail({
+      from: smtpUser,
+      to: toEmail,
+      replyTo: email,
+      subject: `Contato de ${name} - ${company}`,
+      text: `Nome: ${name}\nEmpresa: ${company}\nE-mail: ${email}\nTelefone: ${phone}\n\nMensagem:\n${message}`,
+    })
+    .then((info) => {
+      res.json({ success: true, info });
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar o e-mail:", error);
+      return res.status(500).json({
+        error:
+          "Erro ao enviar o e-mail. Por favor, tente novamente mais tarde. (Código de erro: " +
+          error.code +
+          ")",
+      });
+    });
 });
 
-module.exports = app; // Exporta o aplicativo Express
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
+
+module.exports = app;
